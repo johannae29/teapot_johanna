0a9cb89abbfc3ec8b4826ba37c732d5a5a859ffa
Johanna Erlandsson <johanna.e@gmail.com>
2021-01-03 12:07:10 +0100

    Name display in feed

--- js/main.js
+++ js/main.js
@@ -10,10 +10,14 @@ console.log("i signinForm" + username + password);
 
 	if(username == "Johanna" && password == "password"){
 		
-		setCookie(username);
 		console.log("inuti if");
-		window.location.href = "feed.html";
+		setCookie("username", username, 7);
 		
+
+		//console.log(getCookie("username"));
+
+
+		window.location.href = "feed.html";
 		return false; //vill inte att formuläret ska submitta efter som jag använder location.href
 
 	} else {
@@ -23,16 +27,36 @@ console.log("i signinForm" + username + password);
 		signinErrorMsg.style.opacity = 1;
 		return false;
 	}
+	return false;
 } 
 
+function setCookie(cname,cvalue,exdays) {
+	var d = new Date();
+  	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  	var expires = "expires=" + d.toGMTString();
 
-/*--- Function to set cookie with username ---*/
-function setCookie(username){
-	document.cookie = "username=username";
-	console.log("set cookie" + username);
+
+	console.log("set cookie" + cvalue);
+
+  	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict;";
+
+	console.log(document.cookie);
 }
 
+/*---*/
 
+function setUsername() {
+  var user=getCookie("username");
+
+  console.log("inuti checkCookie " + user);
+
+
+  if (user != "") {
+    document.getElementById("myName").innerHTML = user;
+  } else {
+     window.location.href = "index.html";
+  }
+}
 
 
 
@@ -42,12 +66,32 @@ function setCookie(username){
 
 /*--- Function to get username from cookie ---*/
 function getCookie(username){
-	var name = username + "=";
-	var c;
 
-	if(c.indexOf(name) == 0){
-		return c.substring(name.length, c.length);
+	let name = username + "=";
+	console.log("Användarnamn " + name);
+
+
+	var ca = document.cookie.split(';');
+	console.log(ca);
+
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
 	}
-
 	return "";
+}
+
+
+/*--- Logout, remove cookie---*/
+
+function logout(){
+
+console.log("i logout ");
+	document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;SameSite=Strict;";
+	return false;
 }
