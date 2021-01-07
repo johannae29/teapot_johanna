14d71e6b25c136ebcaae4c0aa9390f5ac4a3f7d6
Johanna Erlandsson <johanna.e@gmail.com>
2021-01-07 21:19:23 +0100

    updated with comments

--- js/main.js
+++ js/main.js
@@ -12,58 +12,54 @@ function signinForm(){
 		//console.log("inuti if");
 		setCookie("username", username, 7);
 
-		window.location.href = "feed.html";
-		return false; //vill inte att formuläret ska submitta efter som jag använder location.href
-
+		window.location.href = "feed.html";//sätter location till feed.html
 	} else {
 		//console.log("inuti else");
 		let signinErrorMsg = document.getElementById("signin-error-msg");
 
-		signinErrorMsg.style.opacity = 1;
-		return false;
+		signinErrorMsg.style.opacity = 1; //gör felmeddelandet synligt
 	}
-	return false;
 } 
 
 /*--- Function to set cookie ---*/
 function setCookie(cname,cvalue,exdays) {
-	var d = new Date();
-  	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  	var expires = "expires=" + d.toGMTString();
+	let d = new Date(); //hämtar aktuell tid
+  	d.setTime(d.getTime() + (exdays*24*60*60*1000)); //sätter giltighetstid i d, 7 dagar framåt
+  	let expires = "expires=" + d.toGMTString(); //skapar en sträng som innehåller expires=<tid i GMT>
 
 	//console.log("set cookie" + cvalue);
 
-  	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict;";
+  	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict;"; //sätter cookie till en sträng 
 
 	//console.log(document.cookie);
 }
 
 /*--- Function to set cookie with username ---*/
 function setUsername() {
-  var user=getCookie("username");
+  let user=getCookie("username");//hämtar värdet på username cookie
 
   if (user != "") {
-    document.getElementById("myName").innerHTML = user;
+    document.getElementById("myName").innerHTML = user;//sätter myName att innehålla värdet i user
   } else {
-     window.location.href = "index.html";
+     window.location.href = "index.html";//redirect till index.html om user saknar värde
   }
 }
 
-/*--- Function to get username from cookie ---*/
-function getCookie(username){
+/*--- Function to get value from cookie ---*/
+function getCookie(cookieName){
 
-	let name = username + "=";
+	let name = cookieName + "=";
 	//console.log("Användarnamn " + name);
 
-	var ca = document.cookie.split(';');
+	let ca = document.cookie.split(';');//delar upp alla cookies på sidan efter ;
 	
-	for(var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
+	for(let i = 0; i < ca.length; i++) { //loopar igenom alla element i ca
+		let c = ca[i];
+		while (c.charAt(0) == ' ') { //tar bort mellanslag från början av strängen c
 			c = c.substring(1);
 		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
+		if (c.indexOf(name) == 0) { //kollar om name finns först i strängen c
+			return c.substring(name.length, c.length);//delar upp strängen c på längden av name och c för att returnera värdet
 		}
 	}
 	return "";
@@ -71,15 +67,15 @@ function getCookie(username){
 
 /*--- Function to remove cookie when logout ---*/
 function logout(){
-	console.log("i logout ");
+	//console.log("i logout ");
 	document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;SameSite=Strict;";
 	//window.location.href = "index.html";
 	//console.log(document.cookie);
 	return true;
 }
 
-/*--- Filter in feed ---*/
 
+/*--- Filter in feed ---*/
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
